7296b2832e22f5c301506c26b0bbf6e0fd665b86
Peter Aigner <mastapetz@icloud.com>
2022-06-08 10:49:36 +0200

    create files, begin basic ex

---
+++ Peter/basic/script.js
@@ -0,0 +1,7 @@
+"use strict";
+// Basic 1 multiplaction 1to10 * i of array
+let numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (let i = 0; i < numberArray.length; i++) {
+    let result = (1) * numberArray[i];
+    console.log(result);
+}
